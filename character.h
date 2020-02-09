bfb6c839f5e9ab4de9815a39a75d859552240e86
jmitche6 <33502053+JohnMitchell493@users.noreply.github.com>
2020-02-08 20:22:22 -0500

    added goldGen

--- character.h
+++ character.h
@@ -11,6 +11,7 @@ private:
 	std::string ClassName;
 	std::string SubClassName;
 	std::string BGName;
+	int gold = 0;
 	int stats[6] = [0, 0, 0, 0, 0, 0];
 	int mods[6] = [0, 0, 0, 0, 0, 0];
 	int BiasArr[6] = [-1, -1, -1, -1, -1, -1];
@@ -1049,6 +1050,35 @@ public:
 			break;
 		}
 	}
+	void genGold(Class) {
+		int gold = 0;
+		if (Class.compare("Barbarian") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Bard") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Cleric") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Druid") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Fighter") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Monk") == 0)
+			gold = ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4) + 1;
+		if (Class.compare("Paladin") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Ranger) == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Rogue") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Sorcerer") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Warlock") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		if (Class.compare("Wizard") == 0)
+			gold = (((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1) + ((rand()) % 4 + 1)) * 10;
+		else
+			gold = 0;
+	}
 	std::string getRaceName() {
 		return this.RaceName;
 	}
@@ -1063,6 +1093,9 @@ public:
 	}
 	std::string getBGName() {
 		return this.BGName;
+	}
+	int getGold() {
+		return this.gold;
 	}
 	void setRaceName(std::string s) {
 		this.RaceName = s;
