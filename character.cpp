bfe4cef329dc684215a721cb4f3fc6321b08fac8
jmitche6 <33502053+JohnMitchell493@users.noreply.github.com>
2020-02-08 20:42:37 -0500

    Update character.cpp

--- character.cpp
+++ character.cpp
@@ -9,3 +9,19 @@
 
 
 // Step 4: Stat Line
+
+std::vector<int> values;
+values.push_back(1);
+values.push_back(3);
+value.push_back(4);
+value.push_back(5);
+value.push_back(-1);
+value.random_shuffle(value.begin, value.end);
+for (int i = 0; i < 5; i++) {
+	BiasArr[i + 2] = value[i];
+}
+for (int i = 0; i < 6; i++) {
+	if (stats[i] != -1) {
+
+	}
+}
